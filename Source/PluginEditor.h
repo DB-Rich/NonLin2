58f743bbb2528253a501047ef4cff944e8956120
origin <richard@two-notes.com>
2023-02-22 20:29:48 +0000

    all parameters in APVTS

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -39,32 +39,52 @@ private:
 
     juce::Slider paramA1;
     juce::Slider paramA2;
+    std::unique_ptr<SliderAttachment> paramA1attachment;
+    std::unique_ptr<SliderAttachment> paramA2attachment;
 
     juce::Slider paramB1;
     juce::Slider paramB2;
+    std::unique_ptr<SliderAttachment> paramB1attachment;
+    std::unique_ptr<SliderAttachment> paramB2attachment;
 
     juce::Slider paramC1;
     juce::Slider paramC2;
+    std::unique_ptr<SliderAttachment> paramC1attachment;
+    std::unique_ptr<SliderAttachment> paramC2attachment;
 
     juce::Slider paramD1;
     juce::Slider paramD2;
+    std::unique_ptr<SliderAttachment> paramD1attachment;
+    std::unique_ptr<SliderAttachment> paramD2attachment;
 
     juce::Slider paramE1;
     juce::Slider paramE2;
+    std::unique_ptr<SliderAttachment> paramE1attachment;
+    std::unique_ptr<SliderAttachment> paramE2attachment;
 
     juce::Slider paramF1;
     juce::Slider paramF2;
+    std::unique_ptr<SliderAttachment> paramF1attachment;
+    std::unique_ptr<SliderAttachment> paramF2attachment;
 
     juce::Slider paramG1;
     juce::Slider paramG2;
+    std::unique_ptr<SliderAttachment> paramG1attachment;
+    std::unique_ptr<SliderAttachment> paramG2attachment;
 
     juce::Slider paramH1;
     juce::Slider paramH2;
+    std::unique_ptr<SliderAttachment> paramH1attachment;
+    std::unique_ptr<SliderAttachment> paramH2attachment;
 
     juce::Slider UI1;
     juce::Slider UI2;
     juce::Slider UI3;
     juce::Slider UI4;
+    std::unique_ptr<SliderAttachment> UI1attachment;
+    std::unique_ptr<SliderAttachment> UI2attachment;
+    std::unique_ptr<SliderAttachment> UI3attachment;
+    std::unique_ptr<SliderAttachment> UI4attachment;
 
     juce::ComboBox slot1;
     juce::ComboBox slot2;
@@ -74,8 +94,17 @@ private:
     juce::ComboBox slot6;
     juce::ComboBox slot7;
     juce::ComboBox slot8;
+    std::unique_ptr<ComboBoxAttachment> slot1attachment;
+    std::unique_ptr<ComboBoxAttachment> slot2attachment;
+    std::unique_ptr<ComboBoxAttachment> slot3attachment;
+    std::unique_ptr<ComboBoxAttachment> slot4attachment;
+    std::unique_ptr<ComboBoxAttachment> slot5attachment;
+    std::unique_ptr<ComboBoxAttachment> slot6attachment;
+    std::unique_ptr<ComboBoxAttachment> slot7attachment;
+    std::unique_ptr<ComboBoxAttachment> slot8attachment;
 
     juce::ComboBox oversample;
+    std::unique_ptr<ComboBoxAttachment> oversampleattachment;
 
     juce::ComboBox option1;
     juce::ComboBox option2;
@@ -85,79 +114,160 @@ private:
     juce::ComboBox option6;
     juce::ComboBox option7;
     juce::ComboBox option8;
+    std::unique_ptr<ComboBoxAttachment> option1attachment;
+    std::unique_ptr<ComboBoxAttachment> option2attachment;
+    std::unique_ptr<ComboBoxAttachment> option3attachment;
+    std::unique_ptr<ComboBoxAttachment> option4attachment;
+    std::unique_ptr<ComboBoxAttachment> option5attachment;
+    std::unique_ptr<ComboBoxAttachment> option6attachment;
+    std::unique_ptr<ComboBoxAttachment> option7attachment;
+    std::unique_ptr<ComboBoxAttachment> option8attachment;
 
 
-    juce::Slider U1Amin;
-    juce::Slider U1Amax;
-    juce::Slider U2Amin;
-    juce::Slider U2Amax;
-    juce::Slider U3Amin;
-    juce::Slider U3Amax;
-    juce::Slider U4Amin;
-    juce::Slider U4Amax;
+    juce::Slider U1AP1;
+    juce::Slider U1AP2;
+    juce::Slider U2AP1;
+    juce::Slider U2AP2;
+    juce::Slider U3AP1;
+    juce::Slider U3AP2;
+    juce::Slider U4AP1;
+    juce::Slider U4AP2;
 
-    juce::Slider U1Bmin;
-    juce::Slider U1Bmax;
-    juce::Slider U2Bmin;
-    juce::Slider U2Bmax;
-    juce::Slider U3Bmin;
-    juce::Slider U3Bmax;
-    juce::Slider U4Bmin;
-    juce::Slider U4Bmax;
+    juce::Slider U1BP1;
+    juce::Slider U1BP2;
+    juce::Slider U2BP1;
+    juce::Slider U2BP2;
+    juce::Slider U3BP1;
+    juce::Slider U3BP2;
+    juce::Slider U4BP1;
+    juce::Slider U4BP2;
 
-    juce::Slider U1Cmin;
-    juce::Slider U1Cmax;
-    juce::Slider U2Cmin;
-    juce::Slider U2Cmax;
-    juce::Slider U3Cmin;
-    juce::Slider U3Cmax;
-    juce::Slider U4Cmin;
-    juce::Slider U4Cmax;
+    juce::Slider U1CP1;
+    juce::Slider U1CP2;
+    juce::Slider U2CP1;
+    juce::Slider U2CP2;
+    juce::Slider U3CP1;
+    juce::Slider U3CP2;
+    juce::Slider U4CP1;
+    juce::Slider U4CP2;
 
-    juce::Slider U1Dmin;
-    juce::Slider U1Dmax;
-    juce::Slider U2Dmin;
-    juce::Slider U2Dmax;
-    juce::Slider U3Dmin;
-    juce::Slider U3Dmax;
-    juce::Slider U4Dmin;
-    juce::Slider U4Dmax;
+    juce::Slider U1DP1;
+    juce::Slider U1DP2;
+    juce::Slider U2DP1;
+    juce::Slider U2DP2;
+    juce::Slider U3DP1;
+    juce::Slider U3DP2;
+    juce::Slider U4DP1;
+    juce::Slider U4DP2;
 
-    juce::Slider U1Emin;
-    juce::Slider U1Emax;
-    juce::Slider U2Emin;
-    juce::Slider U2Emax;
-    juce::Slider U3Emin;
-    juce::Slider U3Emax;
-    juce::Slider U4Emin;
-    juce::Slider U4Emax;
+    juce::Slider U1EP1;
+    juce::Slider U1EP2;
+    juce::Slider U2EP1;
+    juce::Slider U2EP2;
+    juce::Slider U3EP1;
+    juce::Slider U3EP2;
+    juce::Slider U4EP1;
+    juce::Slider U4EP2;
 
-    juce::Slider U1Fmin;
-    juce::Slider U1Fmax;
-    juce::Slider U2Fmin;
-    juce::Slider U2Fmax;
-    juce::Slider U3Fmin;
-    juce::Slider U3Fmax;
-    juce::Slider U4Fmin;
-    juce::Slider U4Fmax;
+    juce::Slider U1FP1;
+    juce::Slider U1FP2;
+    juce::Slider U2FP1;
+    juce::Slider U2FP2;
+    juce::Slider U3FP1;
+    juce::Slider U3FP2;
+    juce::Slider U4FP1;
+    juce::Slider U4FP2;
 
-    juce::Slider U1Gmin;
-    juce::Slider U1Gmax;
-    juce::Slider U2Gmin;
-    juce::Slider U2Gmax;
-    juce::Slider U3Gmin;
-    juce::Slider U3Gmax;
-    juce::Slider U4Gmin;
-    juce::Slider U4Gmax;
+    juce::Slider U1GP1;
+    juce::Slider U1GP2;
+    juce::Slider U2GP1;
+    juce::Slider U2GP2;
+    juce::Slider U3GP1;
+    juce::Slider U3GP2;
+    juce::Slider U4GP1;
+    juce::Slider U4GP2;
 
-    juce::Slider U1Hmin;
-    juce::Slider U1Hmax;
-    juce::Slider U2Hmin;
-    juce::Slider U2Hmax;
-    juce::Slider U3Hmin;
-    juce::Slider U3Hmax;
-    juce::Slider U4Hmin;
-    juce::Slider U4Hmax;
+    juce::Slider U1HP1;
+    juce::Slider U1HP2;
+    juce::Slider U2HP1;
+    juce::Slider U2HP2;
+    juce::Slider U3HP1;
+    juce::Slider U3HP2;
+    juce::Slider U4HP1;
+    juce::Slider U4HP2;
+
+    std::unique_ptr<SliderAttachment> U1AP1attachment;
+    std::unique_ptr<SliderAttachment> U1AP2attachment;
+    std::unique_ptr<SliderAttachment> U2AP1attachment;
+    std::unique_ptr<SliderAttachment> U2AP2attachment;
+    std::unique_ptr<SliderAttachment> U3AP1attachment;
+    std::unique_ptr<SliderAttachment> U3AP2attachment;
+    std::unique_ptr<SliderAttachment> U4AP1attachment;
+    std::unique_ptr<SliderAttachment> U4AP2attachment;
+
+    std::unique_ptr<SliderAttachment> U1BP1attachment;
+    std::unique_ptr<SliderAttachment> U1BP2attachment;
+    std::unique_ptr<SliderAttachment> U2BP1attachment;
+    std::unique_ptr<SliderAttachment> U2BP2attachment;
+    std::unique_ptr<SliderAttachment> U3BP1attachment;
+    std::unique_ptr<SliderAttachment> U3BP2attachment;
+    std::unique_ptr<SliderAttachment> U4BP1attachment;
+    std::unique_ptr<SliderAttachment> U4BP2attachment;
+
+    std::unique_ptr<SliderAttachment> U1CP1attachment;
+    std::unique_ptr<SliderAttachment> U1CP2attachment;
+    std::unique_ptr<SliderAttachment> U2CP1attachment;
+    std::unique_ptr<SliderAttachment> U2CP2attachment;
+    std::unique_ptr<SliderAttachment> U3CP1attachment;
+    std::unique_ptr<SliderAttachment> U3CP2attachment;
+    std::unique_ptr<SliderAttachment> U4CP1attachment;
+    std::unique_ptr<SliderAttachment> U4CP2attachment;
+
+    std::unique_ptr<SliderAttachment> U1DP1attachment;
+    std::unique_ptr<SliderAttachment> U1DP2attachment;
+    std::unique_ptr<SliderAttachment> U2DP1attachment;
+    std::unique_ptr<SliderAttachment> U2DP2attachment;
+    std::unique_ptr<SliderAttachment> U3DP1attachment;
+    std::unique_ptr<SliderAttachment> U3DP2attachment;
+    std::unique_ptr<SliderAttachment> U4DP1attachment;
+    std::unique_ptr<SliderAttachment> U4DP2attachment;
+
+    std::unique_ptr<SliderAttachment> U1EP1attachment;
+    std::unique_ptr<SliderAttachment> U1EP2attachment;
+    std::unique_ptr<SliderAttachment> U2EP1attachment;
+    std::unique_ptr<SliderAttachment> U2EP2attachment;
+    std::unique_ptr<SliderAttachment> U3EP1attachment;
+    std::unique_ptr<SliderAttachment> U3EP2attachment;
+    std::unique_ptr<SliderAttachment> U4EP1attachment;
+    std::unique_ptr<SliderAttachment> U4EP2attachment;
+
+    std::unique_ptr<SliderAttachment> U1FP1attachment;
+    std::unique_ptr<SliderAttachment> U1FP2attachment;
+    std::unique_ptr<SliderAttachment> U2FP1attachment;
+    std::unique_ptr<SliderAttachment> U2FP2attachment;
+    std::unique_ptr<SliderAttachment> U3FP1attachment;
+    std::unique_ptr<SliderAttachment> U3FP2attachment;
+    std::unique_ptr<SliderAttachment> U4FP1attachment;
+    std::unique_ptr<SliderAttachment> U4FP2attachment;
+
+    std::unique_ptr<SliderAttachment> U1GP1attachment;
+    std::unique_ptr<SliderAttachment> U1GP2attachment;
+    std::unique_ptr<SliderAttachment> U2GP1attachment;
+    std::unique_ptr<SliderAttachment> U2GP2attachment;
+    std::unique_ptr<SliderAttachment> U3GP1attachment;
+    std::unique_ptr<SliderAttachment> U3GP2attachment;
+    std::unique_ptr<SliderAttachment> U4GP1attachment;
+    std::unique_ptr<SliderAttachment> U4GP2attachment;
+
+    std::unique_ptr<SliderAttachment> U1HP1attachment;
+    std::unique_ptr<SliderAttachment> U1HP2attachment;
+    std::unique_ptr<SliderAttachment> U2HP1attachment;
+    std::unique_ptr<SliderAttachment> U2HP2attachment;
+    std::unique_ptr<SliderAttachment> U3HP1attachment;
+    std::unique_ptr<SliderAttachment> U3HP2attachment;
+    std::unique_ptr<SliderAttachment> U4HP1attachment;
+    std::unique_ptr<SliderAttachment> U4HP2attachment;
+
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (NonLinAudioProcessorEditor)
 };
--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -29,12 +29,241 @@ NonLinAudioProcessor::NonLinAudioProcessor()
     // parameterID, parameter name,  minimum value, maximum value, default value 
     // must add here if have made an attachment
 
-    //std::make_unique<juce::AudioParameterFloat>("sourceX", "Source X", 15.f, 785.0f, 200.f),//must include offset!
+    std::make_unique<juce::AudioParameterFloat>("parama1", "Param A1", 0.f, 100.0f, 0.f),
+    std::make_unique<juce::AudioParameterFloat>("parama2", "Param A2", 0.f, 100.0f, 0.f),
+    std::make_unique<juce::AudioParameterFloat>("paramb1", "Param B1", 0.f, 100.0f, 0.f),
+    std::make_unique<juce::AudioParameterFloat>("paramb2", "Param B2", 0.f, 100.0f, 0.f),
+    std::make_unique<juce::AudioParameterFloat>("paramc1", "Param C1", 0.f, 100.0f, 0.f),
+    std::make_unique<juce::AudioParameterFloat>("paramc2", "Param C2", 0.f, 100.0f, 0.f),
+    std::make_unique<juce::AudioParameterFloat>("paramd1", "Param D1", 0.f, 100.0f, 0.f),
+    std::make_unique<juce::AudioParameterFloat>("paramd2", "Param D2", 0.f, 100.0f, 0.f),
+    std::make_unique<juce::AudioParameterFloat>("parame1", "Param E1", 0.f, 100.0f, 0.f),
+    std::make_unique<juce::AudioParameterFloat>("parame2", "Param E2", 0.f, 100.0f, 0.f),
+    std::make_unique<juce::AudioParameterFloat>("paramf1", "Param F1", 0.f, 100.0f, 0.f),
+    std::make_unique<juce::AudioParameterFloat>("paramf2", "Param F2", 0.f, 100.0f, 0.f),
+    std::make_unique<juce::AudioParameterFloat>("paramg1", "Param G1", 0.f, 100.0f, 0.f),
+    std::make_unique<juce::AudioParameterFloat>("paramg2", "Param G2", 0.f, 100.0f, 0.f),
+    std::make_unique<juce::AudioParameterFloat>("paramh1", "Param H1", 0.f, 100.0f, 0.f),
+    std::make_unique<juce::AudioParameterFloat>("paramh2", "Param H2", 0.f, 100.0f, 0.f),
+
+    std::make_unique<juce::AudioParameterFloat>("ui1", "UI1", 0.f, 100.0f, 50.f),
+    std::make_unique<juce::AudioParameterFloat>("ui2", "UI2", 0.f, 100.0f, 50.f),
+    std::make_unique<juce::AudioParameterFloat>("ui3", "UI3", 0.f, 100.0f, 50.f),
+    std::make_unique<juce::AudioParameterFloat>("ui4", "UI4", 0.f, 100.0f, 50.f),
+
+    std::make_unique<juce::AudioParameterInt>("slot1", "slot 1", 1, 5, 1), //DEBUG - correct to start at 1 ?
+    std::make_unique<juce::AudioParameterInt>("slot2", "slot 2", 1, 5, 1),
+    std::make_unique<juce::AudioParameterInt>("slot3", "slot 3", 1, 5, 1),
+    std::make_unique<juce::AudioParameterInt>("slot4", "slot 4", 1, 5, 1),
+    std::make_unique<juce::AudioParameterInt>("slot5", "slot 5", 1, 5, 1),
+    std::make_unique<juce::AudioParameterInt>("slot6", "slot 6", 1, 5, 1),
+    std::make_unique<juce::AudioParameterInt>("slot7", "slot 7", 1, 5, 1),
+    std::make_unique<juce::AudioParameterInt>("slot8", "slot 8", 1, 5, 1),
+
+    std::make_unique<juce::AudioParameterInt>("oversample", "oversample", 1, 4, 1),
+
+    std::make_unique<juce::AudioParameterInt>("option1", "option 1", 1, 4, 1),
+    std::make_unique<juce::AudioParameterInt>("option2", "option 2", 1, 4, 1),
+    std::make_unique<juce::AudioParameterInt>("option3", "option 3", 1, 4, 1),
+    std::make_unique<juce::AudioParameterInt>("option4", "option 4", 1, 4, 1),
+    std::make_unique<juce::AudioParameterInt>("option5", "option 5", 1, 4, 1),
+    std::make_unique<juce::AudioParameterInt>("option6", "option 6", 1, 4, 1),
+    std::make_unique<juce::AudioParameterInt>("option7", "option 7", 1, 4, 1),
+    std::make_unique<juce::AudioParameterInt>("option8", "option 8", 1, 4, 1),
+
+    std::make_unique<juce::AudioParameterFloat>("U1AP1", "U1AP1", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U1AP2", "U1AP2", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U2AP1", "U2AP1", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U2AP2", "U2AP2", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U3AP1", "U3AP1", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U3AP2", "U3AP2", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U4AP1", "U4AP1", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U4AP2", "U4AP2", -100.f, 100.0f, 0.f),
+
+std::make_unique<juce::AudioParameterFloat>("U1BP1", "U1BP1", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U1BP2", "U1BP2", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U2BP1", "U2BP1", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U2BP2", "U2BP2", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U3BP1", "U3BP1", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U3BP2", "U3BP2", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U4BP1", "U4BP1", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U4BP2", "U4BP2", -100.f, 100.0f, 0.f),
+
+std::make_unique<juce::AudioParameterFloat>("U1CP1", "U1CP1", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U1CP2", "U1CP2", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U2CP1", "U2CP1", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U2CP2", "U2CP2", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U3CP1", "U3CP1", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U3CP2", "U3CP2", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U4CP1", "U4CP1", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U4CP2", "U4CP2", -100.f, 100.0f, 0.f),
+
+std::make_unique<juce::AudioParameterFloat>("U1DP1", "U1DP1", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U1DP2", "U1DP2", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U2DP1", "U2DP1", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U2DP2", "U2DP2", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U3DP1", "U3DP1", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U3DP2", "U3DP2", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U4DP1", "U4DP1", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U4DP2", "U4DP2", -100.f, 100.0f, 0.f),
+
+std::make_unique<juce::AudioParameterFloat>("U1EP1", "U1EP1", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U1EP2", "U1EP2", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U2EP1", "U2EP1", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U2EP2", "U2EP2", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U3EP1", "U3EP1", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U3EP2", "U3EP2", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U4EP1", "U4EP1", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U4EP2", "U4EP2", -100.f, 100.0f, 0.f),
+
+std::make_unique<juce::AudioParameterFloat>("U1FP1", "U1FP1", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U1FP2", "U1FP2", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U2FP1", "U2FP1", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U2FP2", "U2FP2", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U3FP1", "U3FP1", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U3FP2", "U3FP2", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U4FP1", "U4FP1", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U4FP2", "U4FP2", -100.f, 100.0f, 0.f),
+
+std::make_unique<juce::AudioParameterFloat>("U1GP1", "U1GP1", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U1GP2", "U1GP2", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U2GP1", "U2GP1", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U2GP2", "U2GP2", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U3GP1", "U3GP1", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U3GP2", "U3GP2", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U4GP1", "U4GP1", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U4GP2", "U4GP2", -100.f, 100.0f, 0.f),
+
+std::make_unique<juce::AudioParameterFloat>("U1HP1", "U1HP1", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U1HP2", "U1HP2", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U2HP1", "U2HP1", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U2HP2", "U2HP2", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U3HP1", "U3HP1", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U3HP2", "U3HP2", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U4HP1", "U4HP1", -100.f, 100.0f, 0.f),
+std::make_unique<juce::AudioParameterFloat>("U4HP2", "U4HP2", -100.f, 100.0f, 0.f),
+
+    
+
 
 }) //end of parameters ctor
 
 {
+    parameters.addParameterListener("parama1", this);
+    parameters.addParameterListener("parama2", this);
+    parameters.addParameterListener("paramb1", this);
+    parameters.addParameterListener("paramb2", this);
+    parameters.addParameterListener("paramc1", this);
+    parameters.addParameterListener("paramc2", this);
+    parameters.addParameterListener("paramd1", this);
+    parameters.addParameterListener("paramd2", this);
+    parameters.addParameterListener("parame1", this);
+    parameters.addParameterListener("parame2", this);
+    parameters.addParameterListener("paramf1", this);
+    parameters.addParameterListener("paramf2", this);
+    parameters.addParameterListener("paramg1", this);
+    parameters.addParameterListener("paramg2", this);
+    parameters.addParameterListener("paramh1", this);
+    parameters.addParameterListener("paramh2", this);
+
+    parameters.addParameterListener("ui1", this);
+    parameters.addParameterListener("ui2", this);
+    parameters.addParameterListener("ui3", this);
+    parameters.addParameterListener("ui4", this);
+
+    parameters.addParameterListener("slot1", this);
+    parameters.addParameterListener("slot2", this);
+    parameters.addParameterListener("slot3", this);
+    parameters.addParameterListener("slot4", this);
+    parameters.addParameterListener("slot5", this);
+    parameters.addParameterListener("slot6", this);
+    parameters.addParameterListener("slot7", this);
+    parameters.addParameterListener("slot8", this);
+
+    parameters.addParameterListener("oversample", this);
+
+    parameters.addParameterListener("option1", this);
+    parameters.addParameterListener("option2", this);
+    parameters.addParameterListener("option3", this);
+    parameters.addParameterListener("option4", this);
+    parameters.addParameterListener("option5", this);
+    parameters.addParameterListener("option6", this);
+    parameters.addParameterListener("option7", this);
+    parameters.addParameterListener("option8", this);
+
+    parameters.addParameterListener("U1AP1", this);
+    parameters.addParameterListener("U1AP2", this);
+    parameters.addParameterListener("U2AP1", this);
+    parameters.addParameterListener("U2AP2", this);
+    parameters.addParameterListener("U3AP1", this);
+    parameters.addParameterListener("U3AP2", this);
+    parameters.addParameterListener("U4AP1", this);
+    parameters.addParameterListener("U4AP2", this);
+
+    parameters.addParameterListener("U1BP1", this);
+    parameters.addParameterListener("U1BP2", this);
+    parameters.addParameterListener("U2BP1", this);
+    parameters.addParameterListener("U2BP2", this);
+    parameters.addParameterListener("U3BP1", this);
+    parameters.addParameterListener("U3BP2", this);
+    parameters.addParameterListener("U4BP1", this);
+    parameters.addParameterListener("U4BP2", this);
+
+    parameters.addParameterListener("U1CP1", this);
+    parameters.addParameterListener("U1CP2", this);
+    parameters.addParameterListener("U2CP1", this);
+    parameters.addParameterListener("U2CP2", this);
+    parameters.addParameterListener("U3CP1", this);
+    parameters.addParameterListener("U3CP2", this);
+    parameters.addParameterListener("U4CP1", this);
+    parameters.addParameterListener("U4CP2", this);
+
+    parameters.addParameterListener("U1DP1", this);
+    parameters.addParameterListener("U1DP2", this);
+    parameters.addParameterListener("U2DP1", this);
+    parameters.addParameterListener("U2DP2", this);
+    parameters.addParameterListener("U3DP1", this);
+    parameters.addParameterListener("U3DP2", this);
+    parameters.addParameterListener("U4DP1", this);
+    parameters.addParameterListener("U4DP2", this);
+
+    parameters.addParameterListener("U1EP1", this);
+    parameters.addParameterListener("U1EP2", this);
+    parameters.addParameterListener("U2EP1", this);
+    parameters.addParameterListener("U2EP2", this);
+    parameters.addParameterListener("U3EP1", this);
+    parameters.addParameterListener("U3EP2", this);
+    parameters.addParameterListener("U4EP1", this);
+    parameters.addParameterListener("U4EP2", this);
+
+    parameters.addParameterListener("U1FP1", this);
+    parameters.addParameterListener("U1FP2", this);
+    parameters.addParameterListener("U2FP1", this);
+    parameters.addParameterListener("U2FP2", this);
+    parameters.addParameterListener("U3FP1", this);
+    parameters.addParameterListener("U3FP2", this);
+    parameters.addParameterListener("U4FP1", this);
+    parameters.addParameterListener("U4FP2", this);
+
+    parameters.addParameterListener("U1GP1", this);
+    parameters.addParameterListener("U1GP2", this);
+    parameters.addParameterListener("U2GP1", this);
+    parameters.addParameterListener("U2GP2", this);
+    parameters.addParameterListener("U3GP1", this);
+    parameters.addParameterListener("U3GP2", this);
+    parameters.addParameterListener("U4GP1", this);
+    parameters.addParameterListener("U4GP2", this);
+
+    parameters.addParameterListener("U1HP1", this);
+    parameters.addParameterListener("U1HP2", this);
+    parameters.addParameterListener("U2HP1", this);
+    parameters.addParameterListener("U2HP2", this);
+    parameters.addParameterListener("U3HP1", this);
+    parameters.addParameterListener("U3HP2", this);
+    parameters.addParameterListener("U4HP1", this);
+    parameters.addParameterListener("U4HP2", this);  
 }
+
 
 NonLinAudioProcessor::~NonLinAudioProcessor()
 {
@@ -206,7 +435,10 @@ juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
 }
 
 void NonLinAudioProcessor::parameterChanged(const juce::String& parameterID, float newValue) {
-    if (parameterID == "") {
+    if (parameterID == "parama1") {
+        newValue = newValue; // TODO
+    }
+    else if (parameterID == "parama2") {
         newValue = newValue; // TODO
     }
 }
